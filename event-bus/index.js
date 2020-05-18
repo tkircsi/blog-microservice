a3831863162dd2792d44c0b67a5277d8a976d9bc
Tibor Kircsi <tkircsi@gmail.com>
2020-05-18 22:29:51 +0200

    implement event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+const events = [];
+
 app.post('/events', (req, res, next) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
@@ -15,6 +19,10 @@ app.post('/events', (req, res, next) => {
   axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
@@ -12,13 +13,7 @@ app.use(morgan('dev'));
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -37,10 +32,26 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Query service listening on port 4002!');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log(`Processing event: ${event.type}`);
+
+    handleEvent(event.type, event.data);
+  }
 });
